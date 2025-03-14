782bd570eb81159815eae15a0bcfd9b39feea47e
Cody Mills <CodyMills11@hotmail.com>
2025-03-14 21:51:13 +0000

    Commit #36
    Added Flex boxes to have the slideshow and game description next to each other

--- slideshow.js
+++ slideshow.js
@@ -1,11 +1,6 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
 // Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
